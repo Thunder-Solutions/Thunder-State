76cf2f52e9d0a7cadf79211e241accbc62889938
Jon DeWitt <jon@jonathandewitt.us>
2020-01-10 10:51:58 -0500

    outlining general structure of package

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import sourcemaps from 'rollup-plugin-sourcemaps'
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+	input: 'src/index.js',
+	output: [
+    {
+      file: 'esm/simpleState.min.mjs',
+      format: 'esm',
+      compact: true,
+      sourcemap: true,
+    },
+    {
+      file: 'umd/simpleState.min.js',
+      format: 'umd',
+      name: 'SimpleState',
+      compact: true,
+    },
+    {
+      file: 'dist/simpleState.min.js',
+      format: 'cjs',
+      compact: true,
+    },
+  ],
+	plugins: [
+    sourcemaps(),
+		resolve(),
+		babel({
+			exclude: 'node_modules/**'
+		}),
+		terser(),
+	],
+}
