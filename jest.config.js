779d654dc56896ffbc48fbab070d37c4631e649f
Jon DeWitt <jon@jonathandewitt.us>
2020-01-24 15:44:42 -0500

    using lodash es module instead of node module

--- jest.config.js
+++ jest.config.js
@@ -170,9 +170,9 @@ module.exports = {
   // transform: null,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  transformIgnorePatterns: [
+    "<rootDir>/node_modules/(?!lodash-es/.*)"
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
