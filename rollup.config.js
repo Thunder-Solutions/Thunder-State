e53d628cbe39d391f41f9f00d98a1184743dbbc2
Jon DeWitt <jdewitt@dminc.com>
2020-08-19 16:54:47 -0400

    updating packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 import sourcemaps from 'rollup-plugin-sourcemaps'
 import { terser } from 'rollup-plugin-terser'
 import regenerator from 'rollup-plugin-regenerator'
