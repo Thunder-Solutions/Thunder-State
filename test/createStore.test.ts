9e35c80c2fb2deca84a29785d170dbcfb385154e
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-02-14 00:06:13 +0200

    chore: initial commit

--- test/createStore.test.ts
+++ test/createStore.test.ts
@@ -1,18 +1,19 @@
 import { createStore } from '../src/index';
+import { describe, it, expect } from "vitest"
 
 describe('createStore', () => {
-  it ('creates a state object', () => {
-    const store = createStore({
-      name: 'demo',
-      state: {
-        topLevel: 'hello world',
-        nested: {
-          nested: 'hello world',
-        },
-      },
-    });
-    store.getters.topLevel;
-    store.getters.nested.nested;
-    expect(true).toBe(true)
-  });
+	it('creates a state object', () => {
+		const store = createStore({
+			name: 'demo',
+			state: {
+				topLevel: 'hello world',
+				nested: {
+					nested: 'hello world',
+				},
+			},
+		});
+		store.getters.topLevel;
+		store.getters.nested.nested;
+		expect(true).toBe(true)
+	});
 });
