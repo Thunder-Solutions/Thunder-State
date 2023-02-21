7bb19367cb27f0d0293a957cefe8fa02f3daf093
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2023-02-21 00:00:01 -0500

    updating the example component to have a more thorough testing potential

--- packages/react/test/useStore.example.tsx
+++ packages/react/test/useStore.example.tsx
@@ -1,3 +1,4 @@
+import { useState, FormEventHandler } from 'react';
 import { createStore } from '../../core/src/index';
 import { useStore } from '../src/index';
 
@@ -23,21 +24,29 @@ const UserStore = createStore({
 
 const UseStoreExample = () => {
   const userStore = useStore(UserStore);
-  const handleLogout = async () => {
-    // ... call logout API
+  const handleChangeUser: FormEventHandler = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
     userStore.dispatchers.updateUser({
-      displayName: '',
-      email: '',
+      displayName: formData.get('displayName'),
+      email: formData.get('email'),
     });
   };
 
+  const [msg, setMsg] = useState('No changes yet.');
   userStore.watchers.displayName((newName: string) => {
-    
-  })
+    setMsg(`Changed to: ${newName}`);
+  });
+
   return (
     <>
-      <h1>Welcome back, {userStore.getters.displayName}!</h1>
-      <button onClick={handleLogout}>Log Out</button>
+      <h1>{userStore.getters.displayName}</h1>
+      <p>{msg}</p>
+      <form onSubmit={handleChangeUser}>
+        <input name="displayName" />
+        <input name="email" />
+        <button>Change User</button>
+      </form>
     </>
   );
 }
